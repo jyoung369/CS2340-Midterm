02f65cb3cdb2060b5bf00dc2ed8f6f6d2b8be761
Saniya Savla <ssaniyasavla@gmail.com>
2024-03-08 16:57:48 -0500

    Update AppointmentManager.java

--- AppointmentManager.java
+++ AppointmentManager.java
@@ -17,7 +17,7 @@ public class AppointmentManager {
     private static AppointmentManager instance; 
     private List<User> users;
     private List<Appointment> appointments; 
-
+    
     private AppointmentManager() {
         users = new ArrayList<>();
         appointments = new ArrayList<>();
@@ -31,34 +31,47 @@ public class AppointmentManager {
     }
 
     /**
-     * Schedules an appointment with a doctor for a user.
-     * @param user The user scheduling the appointment.
-     * @param doctor The doctor with whom the appointment is to be scheduled.
-     * @param details The details of the appointment.
-     * @return The scheduled appointment.
+     * schedules an appointment for the user with a doctor
+     * @param user The user scheduling the appointment
+     * @param doctor The doctor with whom the appointment is to be scheduled
+     * @param details The details of the appointment
+     * @return The scheduled appointment
      */
     public Appointment scheduleAppointment(User user, Doctor doctor, String details) {
-        return null;
+        return null; 
     }
 
     /**
-     * Recommends doctors based on user symptoms, discomfort level, and insurance coverage.
-     * @param symptoms List of symptoms described by the user.
-     * @param discomfortLevel The level of discomfort.
-     * @param insuranceCoverage Indicates if the user has insurance coverage.
-     * @return A list of recommended doctors.
+     * This recommends doctors based on their availability and skillset.
+     * @param symptoms The symptoms described by the user
+     * @param discomfortLevel The level of discomfort
+     * @param insuranceCoverage Indicates if the user has insurance coverage
+     * @return A list of recommended doctors
      */
-    public List<Doctor> recommendDoctors(List<String> symptoms, int discomfortLevel, boolean insuranceCoverage) {
+    public List<Doctor> recommendDoctors(String symptoms, int discomfortLevel, boolean insuranceCoverage) {
         return null;
     }
-
     /**
-     * Handles alternative scenarios when the preferred doctor is unavailable, or no matches are found.
-     * @param user The user looking for a doctor.
-     * @param preferences User preferences for finding an alternative.
-     * @return A list of alternative doctors or recommendations.
+     * This recommends alternative doctors if the scheduled doctor is not available
+     * @param user The user scheduling the appointment
+     * @param currentDoctors the list of doctors that the user can currently book from
+     * @return A list of recommended doctors
      */
-    public List<Doctor> handleAlternativeScenarios(User user, String preferences) {
+    public List<Doctor> scheduleAlternativeDoctors(User user, List<Doctor> currentDoctors) {
+        // remove Dr.Young (or any doctor that is not available) 
+        // from currentDoctors and return the list for the user to select from
+        return null;
+    }
+    /**
+     * This recommends alternative doctors if the user wants to change their time
+     * @param user The user scheduling the appointment
+     * @return A list of recommended doctors that the user can schedule with depending on the time
+     */
+    public List<Doctor> scheduleAlternativeTime(User user, String time) {
+        // If the user wants to schedule with a new doctor at a different date then the user can update the date/time with a calendar 
+        // this date will be parsed into a string and fed into this method and this method will query the doctors that are available 
+        // and see which one fits the user's requirements while also being available at the same time/day. This will get all the list of doctors
+        // based on this and return this list. 
         return null;
     }
 }
